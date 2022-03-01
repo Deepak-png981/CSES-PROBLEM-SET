7075db9a27d3af5e7559869a6bd474ad5f7ad47a
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-03-01 14:04:34 +0530

    Formatting

--- Introductory Problems/two_sets.cpp
+++ Introductory Problems/two_sets.cpp
@@ -1,49 +1,54 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 #define ll long long
-int main(){
+int main()
+{
     ll n;
-    cin>>n;
-    //calculating the sum for n natural numbers
-    ll sum = (n*(n+1))/2;
-    //if sum is odd then printing NO
-    if(sum%2 != 0)
+    cin >> n;
+    // calculating the sum for n natural numbers
+    ll sum = (n * (n + 1)) / 2;
+    // if sum is odd then printing NO
+    if (sum % 2 != 0)
     {
-        cout<<"NO"<<endl;
+        cout << "NO" << endl;
         return 0;
     }
-    else{
-        //if sum is even then it will be definitely divide into two parts. so,
-        cout<<"YES"<<endl;
+    else
+    {
+        // if sum is even then it will be definitely divide into two parts. so,
+        cout << "YES" << endl;
 
-        //vectors for storing the numbers 
-        vector<long long int> arr1,arr2;
-        //dividing the total into  half
-        sum = sum/2;
-        while(n){
-            if((sum-n) >= 0){
+        // vectors for storing the numbers
+        vector<long long int> arr1, arr2;
+        // dividing the total into  half
+        sum = sum / 2;
+        while (n)
+        {
+            if ((sum - n) >= 0)
+            {
                 arr1.push_back(n);
                 sum = sum - n;
             }
-            else{
+            else
+            {
                 arr2.push_back(n);
             }
             n--;
         }
-        cout<<arr1.size()<<endl;
-        for(int i = 0; i<arr1.size(); i++){
-            cout<<arr1[i]<<" ";
+        cout << arr1.size() << endl;
+        for (int i = 0; i < arr1.size(); i++)
+        {
+            cout << arr1[i] << " ";
         }
-        cout<<endl;
-        cout<<arr2.size()<<endl;
-        for(int i = 0; i<arr2.size(); i++){
-            cout<<arr2[i]<<" ";
+        cout << endl;
+        cout << arr2.size() << endl;
+        for (int i = 0; i < arr2.size(); i++)
+        {
+            cout << arr2[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
-
-    
 
     return 0;
 }
