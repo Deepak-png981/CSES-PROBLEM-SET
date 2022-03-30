ab5d70615f2606d9317cea5e0330ed117952762b
Deepak Joshi <64543095+Deepak-png981@users.noreply.github.com>
2022-03-30 15:51:38 +0530

    intial commit

--- Sorting and Searching/Ferris_Wheel.cpp
+++ Sorting and Searching/Ferris_Wheel.cpp
@@ -1,53 +1,32 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <unordered_map>
+#include <string.h>
+#include <algorithm>
 #define ll long long
-void solve(){
-    ll n;
-    cin>>n;
-    ll weight;
-    cin>>weight;
+using namespace std;
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
     ll arr[n];
-    for(ll i = 0; i<n; i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    sort(arr , arr + n);
-    ll ans = 0;
-    //failed idea
-    // for(ll i = 1; i<n; i++){
-    //     if((arr[i-1] + arr[i]) <= weight){
-    //         ans += 2;
-    //         i ++;
-    //     }
-    //     else if(arr[i] <= weight){
-    //         // i++;
-    //         ans++;
-    //         i ++;
-    //     }
-    //     else if(arr[i-1] <= weight){
-    //         ans++;
-    //     }
-    //     else if(arr[i] > weight){
-    //         break;
-    //     }
-    // }
-
-    //so basically what we are going to do is , we are using two pointer
-    //method in which we will check the first weight plus the last weight
-    //if it will be less than the variable weight then we will increase
-    //ans and if not then we will simply decrease the last position by 1
-    ll i = 0, j = n-1;
-    while(i <= j){
-        if((arr[i] + arr[j]) <= weight){
+    sort(arr, arr + n);
+    int ans = 0;
+    for(int i = 1; i<n;i++){
+        if(arr[i-1] + arr[i] <= x){
+            ans+=2;
             i++;
-            j--;
-            ans++;
         }
-        else{
-            j--;
-            ans++;
+        else if(arr[i] > x){
+            break;
+        }
+        if(arr[i-1] <= x ){
+            
         }
     }
-    cout<<ans<<endl;
 }
 int main()
 {
